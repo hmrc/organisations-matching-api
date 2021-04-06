a8622637696f7c59d5da959a84b27725adae2110
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-04-06 14:08:29 +0100

    replaced all libs with most up to date versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,22 +7,24 @@ import sbt._
 object AppDependencies {
 
   val hmrc = "uk.gov.hmrc"
+  val hmrcMongo = "uk.gov.hmrc.mongo"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    hmrc                  %% "simple-reactivemongo"       % "7.23.0-play-26",
-    hmrc                  %% "bootstrap-play-26"          % "1.3.0",
-    hmrc                  %% "play-hmrc-api"              % "3.9.0-play-26"
+    hmrcMongo             %% "hmrc-mongo-play-27"         % "0.47.0",
+    hmrc                  %% "bootstrap-backend-play-27"  % "4.1.0",
+    hmrc                  %% "play-hmrc-api"              % "6.2.0-play-27"
   )
 
   def test(scope: String = "test, it") = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "4.1.0"               % scope,
+    hmrc                      %% "bootstrap-test-play-27"   % "4.1.0"               % scope,
+    hmrcMongo                 %% "hmrc-mongo-test-play-27"  % "0.47.0"              % scope,
     "org.scalatest"           %% "scalatest"                % "3.2.5"               % scope,
     "com.typesafe.play"       %% "play-test"                % PlayVersion.current   % scope,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"              % scope,
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"               % scope,
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.62.2"              % scope,
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"               % scope,
     "org.pegdown"             %  "pegdown"                  % "1.6.0"               % scope,
-    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.22.0"              % scope,
+    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.27.0"              % scope,
     "org.mockito"             %  "mockito-core"             % "3.8.0"               % scope
   )
 
