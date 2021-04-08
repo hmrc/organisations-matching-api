75fce7486c45b7fbd9c0af18b9e2c98dd116b5e9
mattyharris1 <matthew.harris@digital.hmrc.gov.uk>
2021-04-08 15:30:19 +0100

    HODS-71 | Added in a documentation controller and version request handler

---
+++ app/uk/gov/hmrc/organisationsmatchingapi/controllers/DocumentationController.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.organisationsmatchingapi.controllers
+
+import controllers.Assets
+import javax.inject.{Inject, Singleton}
+import play.api.Configuration
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.organisationsmatchingapi.views._
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+@Singleton
+class DocumentationController @Inject()(cc: ControllerComponents, assets: Assets, config: Configuration)
+  extends BackendController(cc) {
+
+  def definition(): Action[AnyContent] = Action {
+    Ok(txt.definition())
+      .withHeaders(CONTENT_TYPE -> JSON)
+  }
+  def documentation(
+                     version: String,
+                     endpointName: String
+                   ): Action[AnyContent] =
+    assets.at(s"/public/api/documentation/$version", s"${endpointName.replaceAll(" ", "-")}.xml")
+
+  def raml(version: String, file: String) =
+    assets.at(s"/public/api/conf/$version", file)
+
+}
