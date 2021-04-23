74489e7acc1c9e27997090302af0f7212b1dcd12
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-04-23 15:34:54 +0100

    HODS-78 | Sorted basic audits, added audit specs

---
+++ app/uk/gov/hmrc/organisationsmatchingapi/audit/AuditHelper.scala
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.organisationsmatchingapi.audit
+
+import play.api.mvc.RequestHeader
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.organisationsmatchingapi.audit.models._
+import uk.gov.hmrc.play.audit.http.connector.AuditConnector
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class AuditHelper @Inject()(auditConnector: AuditConnector)
+                           (implicit ec: ExecutionContext) {
+
+  def auditApiResponse(correlationId: String,
+                       matchId: String,
+                       scopes: String,
+                       request: RequestHeader,
+                       selfLink: String)
+                      (implicit hc: HeaderCarrier) =
+    auditConnector.sendExplicitAudit(
+      "ApiResponseEvent",
+      ApiResponseEventModel(
+        deviceId = hc.deviceID.getOrElse("-"),
+        input = s"Request to ${request.path}",
+        method = request.method.toUpperCase,
+        userAgent = request.headers.get("User-Agent").getOrElse("-"),
+        apiVersion = "1.0",
+        matchId = matchId,
+        correlationId = Some(correlationId),
+        scopes = scopes,
+        returnLinks = selfLink
+      )
+    )
+
+  def auditApiFailure(correlationId: Option[String],
+                      matchId: String,
+                      request: RequestHeader,
+                      requestUrl: String,
+                      msg: String)
+                     (implicit hc: HeaderCarrier) =
+    auditConnector.sendExplicitAudit(
+      "ApiFailureEvent",
+      ApiFailureResponseEventModel(
+        deviceId = hc.deviceID.getOrElse("-"),
+        input = s"Request to ${request.path}",
+        method = request.method.toUpperCase,
+        userAgent = request.headers.get("User-Agent").getOrElse("-"),
+        apiVersion = "1.0",
+        matchId = matchId,
+        correlationId = correlationId,
+        requestUrl,
+        msg
+      )
+    )
+
+  def auditIfApiResponse(correlationId: String,
+                         matchId: String,
+                         request: RequestHeader,
+                         requestUrl: String,
+                         ifDetailsResponse: String)
+                        (implicit hc: HeaderCarrier) =
+    auditConnector.sendExplicitAudit(
+      "IntegrationFrameworkApiResponseEvent",
+      IfApiResponseEventModel(
+        deviceId = hc.deviceID.getOrElse("-"),
+        input = s"Request to ${request.path}",
+        method = request.method.toUpperCase,
+        userAgent = request.headers.get("User-Agent").getOrElse("-"),
+        apiVersion = "1.0",
+        matchId = matchId,
+        correlationId = correlationId,
+        requestUrl = requestUrl,
+        ifResponse = ifDetailsResponse
+      )
+    )
+
+  def auditIfApiFailure(correlationId: String,
+                        matchId: String,
+                        request: RequestHeader,
+                        requestUrl: String,
+                        msg: String)
+                       (implicit hc: HeaderCarrier) =
+    auditConnector.sendExplicitAudit(
+      "IntegrationFrameworkApiFailureEvent",
+      ApiFailureResponseEventModel(
+        deviceId = hc.deviceID.getOrElse("-"),
+        input = s"Request to ${request.path}",
+        method = request.method.toUpperCase,
+        userAgent = request.headers.get("User-Agent").getOrElse("-"),
+        apiVersion = "1.0",
+        matchId = matchId,
+        correlationId = Some(correlationId),
+        requestUrl,
+        msg
+      )
+    )
+
+  def auditAuthScopes(matchId: String,
+                      scopes:  String,
+                      request: RequestHeader)
+                     (implicit hc: HeaderCarrier) =
+    auditConnector.sendExplicitAudit(
+      "AuthScopesAuditEvent",
+      ScopesAuditEventModel(
+        deviceId = hc.deviceID.getOrElse("-"),
+        input = s"Request to ${request.path}",
+        method = request.method.toUpperCase,
+        userAgent = request.headers.get("User-Agent").getOrElse("-"),
+        apiVersion = "1.0",
+        matchId = matchId,
+        scopes
+      )
+    )
+}
---
+++ test/unit/uk/gov/hmrc/organisationsmatchingapi/audit/models/ScopesAuditEventModelSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.uk.gov.hmrc.organisationsmatchingapi.audit.models
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.libs.json.Json
+import uk.gov.hmrc.organisationsmatchingapi.audit.models.ScopesAuditEventModel
+
+class ScopesAuditEventModelSpec extends AnyWordSpec with Matchers {
+  "Serializes to Json Correctly" in {
+    val ifApiResponseEventModel = ScopesAuditEventModel(
+      "test",
+      "test",
+      "test",
+      "test",
+      "test",
+      "test",
+      "test"
+    )
+
+    val result = Json.toJson(ifApiResponseEventModel)
+
+    result shouldBe Json.obj(
+      "deviceId" -> "test",
+      "input" -> "test",
+      "method" -> "test",
+      "userAgent" -> "test",
+      "apiVersion" -> "test",
+      "matchId" -> "test",
+      "scopes" -> "test"
+    )
+  }
+
+  "Deserializes to Json Correctly" in {
+    val json = Json.obj(
+      "deviceId" -> "test",
+      "input" -> "test",
+      "method" -> "test",
+      "userAgent" -> "test",
+      "apiVersion" -> "test",
+      "matchId" -> "test",
+      "scopes" -> "test"
+    )
+
+    val result = Json.fromJson[ScopesAuditEventModel](json)
+
+    result.get shouldBe ScopesAuditEventModel(
+      "test",
+      "test",
+      "test",
+      "test",
+      "test",
+      "test",
+      "test"
+    )
+  }
+}
