6be2f3ab3f8f68746aa294dbca69a1f8d7bcc9e1
ajchilts <40767309+ajchilts@users.noreply.github.com>
2021-04-26 16:52:26 +0100

    Add comments to options for matching

--- app/uk/gov/hmrc/organisationsmatchingapi/services/AddressMatchingService.scala
+++ app/uk/gov/hmrc/organisationsmatchingapi/services/AddressMatchingService.scala
@@ -4,6 +4,8 @@ import uk.gov.hmrc.organisationsmatchingapi.models.Address
 
 class AddressMatchingService {
 
+  // Option 1 will try a series of data cleanses with a match attempt in between.
+  // This means we could potentially trigger a match prior to manipulating the data too much.
   def matchAddress(knownAddress: Address, ifAddress: Address) = {
     knownAddress.ignoreCase match {
       case ifAddress.ignoreCase => true
@@ -26,6 +28,8 @@ class AddressMatchingService {
     }
   }
 
+  // Option two fully cleanses (manipulates) the data prior to the match.
+  // This means we may get a quicker match however; we have heavily manipulated the data. Possibly unnecessarily.
   def matchAddressCleanAll(knownAddress: Address, ifAddress: Address) = {
     knownAddress.cleanAll match {
       case ifAddress.cleanAll => true
