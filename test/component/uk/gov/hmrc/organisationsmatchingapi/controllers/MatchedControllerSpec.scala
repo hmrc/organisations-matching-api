e6409991bbe360df4610ec17453ee1172a3701a4
feyyazrc <95756811+feyyazrc@users.noreply.github.com>
2023-10-25 11:42:01 +0100

    Revert "Disable vat endpoints (#103)"
    This reverts commit 28ec3250a8c0320d3274d6b7950d59d061ee72de.

--- test/component/uk/gov/hmrc/organisationsmatchingapi/controllers/MatchedControllerSpec.scala
+++ test/component/uk/gov/hmrc/organisationsmatchingapi/controllers/MatchedControllerSpec.scala
@@ -330,7 +330,7 @@ class MatchedControllerSpec extends BaseSpec  {
     }
   }
 
-  ignore("vat") {
+  Feature("vat") {
     Scenario("a valid request is made for an existing match") {
       Given("A valid privileged Auth bearer token")
       AuthStub.willAuthorizePrivilegedAuthToken(authToken, scopes)
